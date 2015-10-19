44075e7ea54d74db59b9e004debe235985e88d89
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2015-10-19 22:49:56 +0200

    Better parameter handling for cache-contra directives

--- lib/Dancer2/Plugin/HTTP/Caching.pm
+++ lib/Dancer2/Plugin/HTTP/Caching.pm
@@ -11,7 +11,7 @@ use HTTP::Date;
 # RFC 7234: ¤ 5.3.      Expires
 register http_expire => sub {
     $_[0]->log( warning =>
-        "Missing date for HTTP Header-Field 'Expire'" )
+        "http_expire: missing date" )
         unless  $_[1];
     $_[0]->header('Expires' => $_[1]);
     return;
@@ -19,73 +19,61 @@ register http_expire => sub {
 
 # RFC 7234: ¤ 5.2.2.1.  must-revalidate
 register http_cache_must_revalidate => sub {
-    _append_cache_control('must-revalidate' => @_);
+        shift->_append_cache_control_directive('must-revalidate' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.2.  no-cache
 register http_cache_no_cache => sub {
-    $_[0]->log( warning =>
-        "Cache-Control Directive 'no-cache' does not support fields (yet)" )
-        if defined $_[1];
-    _append_cache_control('no-cache' => @_);
+    shift->_append_cache_control_directive_quoted('no-cache' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.3.  no-store
 register http_cache_no_store => sub {
-    _append_cache_control('no-store' => @_);
+    shift->_append_cache_control_directive('no-store' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.4.  no-transform
 register http_cache_no_transform => sub {
-    _append_cache_control('no-transform' => @_);
+    shift->_append_cache_control_directive('no-transform' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.5.  public
 register http_cache_public => sub {
-    _append_cache_control('public' => @_);
+    shift->_append_cache_control_directive('public' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.6.  private
 register http_cache_private => sub {
-    $_[0]->log( warning =>
-        "Cache-Control Directive 'private' does not support fields (yet)" )
-        if defined $_[1];
-    _append_cache_control('private' => @_);
+    shift->_append_cache_control_directive_quoted('private' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.7.  proxy-revalidate
 register http_cache_proxy_revalidate => sub {
-    _append_cache_control('proxy-revalidate' => @_);
+    shift->_append_cache_control_directive('proxy-revalidate' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.8.  max-age
 register http_cache_max_age => sub {
-    $_[0]->log( warning =>
-        "Cache-Control Directive 'max-age' missing delta-seconds" )
-        unless defined $_[1];
-    _append_cache_control('max-age' => @_);
+    shift->_append_cache_control_directive_seconds('max-age' => @_);
     return;
 };
 
 # RFC 7234: ¤ 5.2.2.9.  s-maxage
 register http_cache_s_maxage => sub {
-    $_[0]->log( warning =>
-        "Cache-Control Directive 's-maxage' missing delta-seconds" )
-        unless defined $_[1];
-    _append_cache_control('s-maxage' => @_);
+    shift->_append_cache_control_directive_seconds('s-maxage' => @_);
     return;
 };
 
 sub _append_cache_control {
-    my $directive   = shift;
     my $dsl         = shift;
+    my $directive   = shift;
     my $value       = shift;
     
     $dsl->header('Cache-Control' =>
@@ -94,7 +82,47 @@ sub _append_cache_control {
             ( defined $value ? join '=', $directive, $value : $directive)
     );
     return $dsl->header('Cache-Control')
-;}
+};
+
+sub _append_cache_control_directive {
+    my $dsl         = shift;
+    my $directive   = shift;
+    
+    $dsl->log( warning =>
+        "http_cache_control: '$directive' does not take any parameters"
+    ) if @_ ;
+    
+    return $dsl->_append_cache_control($directive, undef);
+};
+
+sub _append_cache_control_directive_seconds {
+    my $dsl         = shift;
+    my $directive   = shift;
+    my $seconds     = shift || 0;
+    
+    $dsl->log( warning =>
+        "http_cache_control: '$directive' does only take 'delta-seconds'"
+    ) if @_ ;
+    
+    $dsl->log( error =>
+        "http_cache_control: '$directive' requires number of seconds"
+    ) unless $seconds =~ /\d+/ ;
+    
+    my $value = $seconds;
+    return $dsl->_append_cache_control($directive, $value);
+};
+
+sub _append_cache_control_directive_quoted {
+    my $dsl         = shift;
+    my $directive   = shift;
+    my @strings     = ref $_[0] eq 'ARRAY' ? @$_[0] : @_; 
+#   my @strings     = @_; 
+    
+    my $value = @strings ? '"' . join(' ', @strings) . '"' : undef;
+    return $dsl->_append_cache_control($directive, $value);
+};
+
+
 
 on_plugin_import {
     my $dsl = shift;
